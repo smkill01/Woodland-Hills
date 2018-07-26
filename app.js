2ce0dd7d4354a78acdd45f4fb9352397c57009b1
Sean Killeen <smkilleen86@gmail.com>
2018-07-25 20:07:04 -0400

    updated js

---
+++ app.js
@@ -0,0 +1,18 @@
+//This function expands an element when clicked
+function myfunction() {
+  var acc = document.getElementsByClassName("accordion");
+    var i;
+    
+    for (i = 0; i < acc.length; i++) {
+      acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight){
+          panel.style.maxHeight = null;
+        } else {
+          panel.style.maxHeight = panel.scrollHeight + "px";
+        } 
+      });
+    }
+}
+myfunction();
